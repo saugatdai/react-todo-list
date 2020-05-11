b8439d61c60714aad46caf4371573f92fbe195ed
saugatdai <saugatdai@gmail.com>
2020-05-11 13:48:21 +0545

    initial commit

---
+++ src/components/TodoInput.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class TodoInput extends Component {
+    render() {
+        return (
+            <h1>Holus From todo Input</h1>
+        )
+    }
+}
